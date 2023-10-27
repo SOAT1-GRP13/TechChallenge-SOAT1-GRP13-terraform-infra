8df57012d4f4e5bb9e6248be35a326d67146d4c2
luiz-soh <luizsouza@MacBook-Pro-de-Luiz.local>
2023-10-27 14:30:57 -0300

    ajustando formatação

--- variables.tf
+++ variables.tf
@@ -19,20 +19,6 @@ variable "profile" {
 }
 
 //Networking
-variable "vpc_cidr" {
-  description = "The CIDR block of the vpc"
-}
-
-variable "public_subnets_cidr" {
-  description = "The CIDR block for the public subnet"
-}
-
-variable "private_subnets_cidr" {
-  description = "The CIDR block for the private subnet"
-}
-variable "availability_zones" {
-  description = "availability zone to the subnets"
-}
 
 variable "db_username" {
   description = "us-west-2"
