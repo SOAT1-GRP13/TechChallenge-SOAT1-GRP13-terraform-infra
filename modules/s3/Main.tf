a55a16e359a8e2848ed31d32cb2c431e2de2de19
luiz-soh <luizsouza@MBP-de-Luiz.Dlink>
2023-11-04 10:33:43 -0300

    teste_pr

--- modules/s3/Main.tf
+++ modules/s3/Main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "lambda_s3" {
     Environment = "${var.environment}"
   }
 }
+
+resource "aws_s3_bucket" "lambda_s3_2" {
+  bucket = "tech-challenge-soat1-grp13-s3-auth-te"
+
+  tags = {
+    Name        = "S3-lambda"
+    Environment = "${var.environment}"
+  }
+}
